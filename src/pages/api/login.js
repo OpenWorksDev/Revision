ef78e9b1b9510066d7605e8c0dbdeb9aab5deaf6
GreyStinger <greystinger@gmail.com>
2022-10-25 16:43:28 +0200

    added null return for login api

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,8 +1,10 @@
 let db = require("../../config/db");
 
-export default function LoginAPIRoute() {
+export default function LoginAPIRoute(req, res) {
   if (req.method !== "POST") {
     res.status(405).end();
     return;
   }
+
+  res.status(200).end();
 }
