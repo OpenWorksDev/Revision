a05165f2e66808396e6f0e1c512619ef3390a430
veryCrunchy <verycrunchydev@gmail.com>
2022-10-25 21:17:10 +0200

    Update login.js

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,6 +1,13 @@
-let db = require("../../config/db");
+let r = require("../../middleware/db");
+let { hash } = require("../../components/authentication");
+import { Worker as snow } from "../../components/snowflake";
+const flake = new snow();
+// const { Worker } = require('snowflake-uuid');
 
 export default function LoginAPIRoute(req, res) {
+  let data = JSON.parse(req.body);
+  console.log(hash(data.passwd));
+
   if (req.method !== "POST") {
     res.status(405).end();
     return;
