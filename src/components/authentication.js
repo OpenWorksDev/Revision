b6c259ab0b822fe7de0155767cc0336ef9b899e5
veryCrunchy <verycrunchydev@gmail.com>
2022-10-27 19:41:45 +0200

    Db fix

--- src/components/authentication.js
+++ src/components/authentication.js
@@ -1,13 +1,19 @@
-const bcrypt = require("bcrypt");
+import { hash as _hash, compare as _compare } from "bcrypt";
 async function hash(password) {
   console.log(password);
-  let hash = await bcrypt.hash(password, 10);
+  let hash = await _hash(password, 10);
   return hash;
 }
 async function compare(password, hash) {
   console.log(password);
-  let comp = await bcrypt.compare(password, hash);
+  let comp = await _compare(password, hash);
   return comp;
 }
 
-module.exports = { hash, compare };
+function formJSON(formData) {
+  var formObj = {};
+  formData.forEach((value, key) => (formObj[key] = value));
+  return JSON.stringify(formObj);
+}
+
+export { hash, compare, formJSON };
--- src/pages/api/validation/[uname].js
+++ src/pages/api/validation/[uname].js
@@ -1,10 +1,16 @@
+let { creds, r } = require("../../../../lib/db");
+
 export default function UnameCheck(req, res) {
-	const { uname } = req.query;
-	// TODO: Username Query Check
-	// Query the database for usernames in use
-	// Only return true if no one else has said username
+  const { uname } = req.query;
 
+  // TODO: Username Query Check
+  // Query the database for usernames in use
+  // Only return true if no one else has said username
+  r.connect(creds, function (err, conn) {
+    console.log(r.table("users").filter({ username: uname }).run(conn));
+  });
+  console.log(uname);
 
-	// Currently defaults to a used username thus not allowing call to register.js
-	res.status(200).send({valid: false});
+  // Currently defaults to a used username thus not allowing call to register.js
+  res.status(200).send({ valid: true });
 }
