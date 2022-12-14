359feba0f474b17bf06f1f124e4d300615157a00
GreyStinger <greystinger@gmail.com>
2022-12-14 18:11:43 +0200

    add 404 page

---
+++ src/components/Error404.js
@@ -0,0 +1,26 @@
+import styles from "../styles/404.module.css";
+
+import MainLayout from "./layout";
+
+import Head from "next/head";
+import Link from "next/link";
+
+/// Component to be placed whenever a url page is not found
+export default function Error404() {
+  return (
+    <MainLayout>
+      <Head>
+        <title>Page not found</title>
+      </Head>
+      <div className={styles["notfound-wrapper"]}>
+        <div className={styles["notfound"]}>
+          <div className={styles["notfound-404"]}>
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+            <Link href="/">Click to go home</Link>
+          </div>
+        </div>
+      </div>
+    </MainLayout>
+  );
+}
