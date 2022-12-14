5431d8a1829fd1b705293e85d3dfe6e693bb7a4e
GreyStinger <greystinger@gmail.com>
2022-12-14 18:11:59 +0200

    add main layout component

---
+++ src/components/layout.js
@@ -0,0 +1,34 @@
+import styles from "../styles/layout.module.css";
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default function MainLayout({ children }) {
+  return (
+    <div style={{minHeight: "100vh"}}>
+      <div className={styles["main-header"]}>
+        <div className={styles["header-image"]}>
+          <Image alt="fav" src="/favicon.ico" height="40px" width="40px" />
+        </div>
+        <div className={styles["header-item"]}>
+          <Link href="/home">Home</Link>
+        </div>
+        <div className={styles["header-item"]}>
+          <Link href="/library">Library</Link>
+        </div>
+        <div className={styles.spacer} /> {/* Spacer */}
+        <div className={styles["header-item"]}>
+          <Link href="/logout">Logout</Link>
+        </div>
+      </div>
+
+      { children }
+
+      <div className={styles["layout-footer"]}>
+        <div>
+          <p>&copy; { new Date().getFullYear() }</p>
+        </div>
+      </div>
+    </div>
+  );
+}
