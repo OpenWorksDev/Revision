0ec0625ec4b862c625e9bf1676c53ab510f79b9f
veryCrunchy <verycrunchydev@gmail.com>
2023-01-22 23:37:50 +0100

    Configure nProgress

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,31 @@
 import "../styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import NProgress from "nprogress";
+import "../styles/nprogress.css";
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    NProgress.configure({ showSpinner: false });
+    const handleStart = () => {
+      NProgress.start();
+    };
+
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router]);
   return <Component {...pageProps} />;
 }
-
-export default MyApp;
